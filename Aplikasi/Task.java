b5eb6b2d5c6c3ac71a5a0ff37ca14eb0d25c9c2b
anandaalex <11221032@student.itk.ac.id>
2023-06-18 19:59:52 +0800

    Update class Task

--- Aplikasi/Task.java
+++ Aplikasi/Task.java
@@ -1,5 +1,37 @@
 package Aplikasi;
 
-public class Task {
+import java.util.Date;
+
+public class Task extends Schedule{
+    private Date dueDate;
+    private boolean statusComplete;
+
+    public Task(String title, Date dueDate) {
+        setTitle(title);
+        this.dueDate = dueDate;
+        this.statusComplete = false;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public boolean isStatusComplete() {
+        return statusComplete;
+    }
+
+    public void setEndTask(boolean completed) {
+        this.statusComplete = completed;
+    }
+
+    public void status() {
+        System.out.println("Task: " + getTitle());
+        System.out.println("Due Date: " + dueDate);
+        System.out.println("Status: " + (statusComplete ? "Completed" : "Incomplete"));
+    }
 
 }
