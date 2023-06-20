36d2cd893a7b87463bbf8df79108fbf70cbfc8ab
Felix1180 <11221080@student.itk.ac.id>
2023-06-20 18:45:30 +0800

    Menambahkan class Reminder

--- Aplikasi/Reminder.java
+++ Aplikasi/Reminder.java
@@ -1,5 +1,51 @@
 package Aplikasi;
 
-public class Reminder {
+import java.time.LocalDate;
+import java.util.Date;
 
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+
+public class Task extends Schedule {
+	private LocalDate taskDate;
+    private Date dueDate;
+    private boolean statusComplete;
+
+    public Task(String title,Date dueDate, LocalDate taskDate,String priority) {
+        setTitle(title);
+        setPriority(priority);
+        this.taskDate = taskDate;
+        this.dueDate = dueDate;
+        this.statusComplete = false;
+    }
+    public LocalDate getTaskDate() {
+		return taskDate;
+	}
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public boolean isStatusComplete() {
+        return statusComplete;
+    }
+
+    public void setEndTask(boolean completed) {
+        this.statusComplete = completed;
+    }
+    
+   
+    
+    public void status() {
+        System.out.println("Task: " + getTitle());
+        System.out.println("Due Date: " + dueDate);
+        System.out.println("Status: " + (statusComplete ? "Completed" : "Incomplete"));
+    }
+
+	
 }
+
