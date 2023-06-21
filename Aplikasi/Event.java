3e48771f06ac2a9af2efdf300a9b496b24f23341
Felix1180 <11221080@student.itk.ac.id>
2023-06-21 15:12:02 +0800

    Memperbarui class Event

--- Aplikasi/Event.java
+++ Aplikasi/Event.java
@@ -1,40 +1,50 @@
 package Aplikasi;
 
+import java.time.LocalDate;
 import java.util.Date;
 
-public class Event extends Schedule{
+public class Event extends Schedule {
+	
     private String location;
     private Date startDate;
     private Date endDate;
+    private LocalDate eventDate;
 
-
-    public Event(String title, Date startDate, Date endDate){
+    public Event(String title, Date startDate, Date endDate, String location, LocalDate eventDate,String priority) {
         setTitle(title);
+        setPriority(priority);
         this.startDate = startDate;
         this.endDate = endDate;
+        this.location = location;
+        this.eventDate = eventDate;
     }
+    public LocalDate getEventDate() {
+		return eventDate;
+	}
 
-    public String getLocation(){
+    public String getLocation() {
         return location;
     }
 
-    public void setLocation(String location){
+    public void setLocation(String location) {
         this.location = location;
     }
 
-    public Date getStarDate(){
+    public Date getStartDate() {
         return startDate;
     }
 
-    public void setStartDate(Date startDate){
+    public void setStartDate(Date startDate) {
         this.startDate = startDate;
+
     }
 
-    public Date getEndDate(){
+    public Date getEndDate() {
         return endDate;
     }
 
-    public void setEndDate(Date endDate){
+    public void setEndDate(Date endDate) {
         this.endDate = endDate;
     }
+	
 }
