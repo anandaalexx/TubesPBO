1aa753367b349668c77f6e509a8666e8500d04f2
Ince Iqbal Aulia Salam <11221016@student.itk.ac.id>
2023-06-21 19:26:15 +0800

    update class Main

--- Aplikasi/Main.java
+++ Aplikasi/Main.java
@@ -1,5 +1,7 @@
 package Aplikasi;
 
+import java.util.TimeZone;
+
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.geometry.Insets;
@@ -8,13 +10,19 @@ import javafx.scene.Scene;
 import javafx.scene.layout.*;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
+import java.util.Date;
 
 public class Main extends Application {
+    private Calendar calendar;
     public static void main(String[] args) {
         Application.launch(args);
     }
 
     public void start(Stage primaryStage) {
         
+        primaryStage.setTitle("Sistem Informasi Penjadwalan dan Task Manajemen");
+        Date currentDate = new Date();
+        TimeZone timeZone = TimeZone.getDefault();
+        calendar = new Calendar(currentDate, timeZone);
     }
 }
