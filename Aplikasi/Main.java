52a979b1971d12f7573290faaf28cb63d5d3cb16
YunnifaNL <yui4yunnifa@gmail.com>
2023-06-21 21:52:54 +0800

    menambah method showAlert

--- Aplikasi/Main.java
+++ Aplikasi/Main.java
@@ -481,6 +481,14 @@ public class Main extends Application {
         }
     }
 
+    private void showAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
 
 }
 
