7445a26188a1230fda730da4d11ce459afacbdab
anandaalex <11221032@student.itk.ac.id>
2023-06-21 19:20:10 +0800

    update class Main

---
+++ Aplikasi/Main.java
@@ -0,0 +1,20 @@
+package Aplikasi;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    public void start(Stage primaryStage) {
+        
+    }
+}
